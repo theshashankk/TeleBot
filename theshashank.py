ca2ca7653fe1ae277bb13fd12c3839a75c3de89c
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-26 19:54:52 +0530

    Create theshashank.py

---
+++ theshashank.py
@@ -0,0 +1,3 @@
+#To Solve Telebot Suspension problem by @xD_Shashank
+import os
+os.system("git clone https://github.com/theshashankk/TeleBot /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
