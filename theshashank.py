2d9cd39047a6a4a5711bc721542ac3ec454e357d
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-26 19:56:10 +0530

    Delete theshashank.py

--- theshashank.py
+++
@@ -1,3 +0,0 @@
-#To Solve Telebot Suspension problem by @xD_Shashank
-import os
-os.system("git clone https://github.com/theshashankk/TeleBot /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
