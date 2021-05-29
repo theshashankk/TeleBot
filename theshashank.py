e3eb24adb25a8640f2e186591c71b417a15c8a8a
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-29 21:21:19 +0530

    Update theshashank.py

--- theshashank.py
+++ theshashank.py
@@ -1,3 +1,17 @@
-#To Solve Telebot Suspension problem by @xD_Shashank
-import os
-os.system("git clone https://github.com/theshashankk/TeleBot /root/telebot && mkdir /root/telebot/bin/ && cd /root/telebot/ && chmod +x /usr/local/bin* && python3 -m telebot")
+import os 
+import subprocess
+from logging import DEBUG, INFO, basicConfig, getLogger, warning
+basicConfig(format="𝐓𝐇𝐄 𝐒𝐇𝐀𝐒𝐇𝐀𝐍𝐊 %(asctime)s ✘ - ⫸ %(name)s ⫷ - ⛝ %(levelname)s ⛝ - ║ %(message)s ║", level=INFO)
+LOGS = getLogger("Helper")
+os.system("git clone https://github.com/theshashankk/TeleBot telebot")
+os.chdir("darkcobra")
+process = subprocess.Popen(
+        ["python3", "-m", "userbot"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,)
+out, er = process.communicate()
+if er:
+    LOGS.warning(er.decode())
+print("::::::::::::::")
+if out:
+    LOGS.info(out.decode())
